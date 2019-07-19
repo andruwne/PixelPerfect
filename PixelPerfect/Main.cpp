f8444e14a279d26ab7409c5592eee70a49542802
Andruw Earnhardt <andruw.earnhardt@sealevel.com>
2019-07-19 17:22:47 -0400

    Handled different tests

--- PixelPerfect/Main.cpp
+++ PixelPerfect/Main.cpp
@@ -1,4 +1,6 @@
 #include "include.h"
+#include "pixel.h"
+#include "line.h"
 
 #define LANGUAGE CPP
 
@@ -8,114 +10,198 @@ const int WIDTH = 640;
 const int HEIGHT = 480;
 const int FLAGS = SDL_WINDOW_SHOWN;
 
-void print_header(const char* header);
-void pixel_test();
-int start(int argc, char* argv[]);
+int errnum;
 
-int start(int argc, char* argv[]) {
-	int width = 0;
-	int height = 0;
-	int bpp = 0;
-	int flags = 0;
+SDL_Window* window;
+SDL_Renderer* renderer;
+SDL_Texture* texture;
+SDL_Surface* bmp;
+SDL_Event event;
 
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		fprintf(stderr, "Video initialization failed: %s\n",
-			SDL_GetError());
-		SDL_Quit();
-		return 1;
-	}
+int pixel_test ();
+int image_test ();
+int line_test ();
+int setup ();
+int input ();
 
-	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
-	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
-	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+int main (int argc, char* argv[]) {
+    errnum == 0;
+    char* test;
+    char* arg_check; memcpy (arg_check, argv[1], 8);
+    if (argc < 1 || strcmp (arg_check, "--test=") != 0) {
+        test = argv[1];
+    } else {
+        perror ("At least one test must be specified using --test=");
+        errnum == -1;
+    }
 
-	SDL_Window* win = SDL_CreateWindow("PixelPerfect", 100, 100, WIDTH, HEIGHT, flags);
-	if (win == nullptr) {
-		fprintf(stderr, "Video mode set failed: %s\n",
-			SDL_GetError());
-		SDL_Quit();
-		return 1;
-	}
+    errnum == setup ();
+    if (errnum != 0) {
+        return exit () || errnum;
+    }
 
-	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (ren == nullptr) {
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
+    if (strcmp (test, "--test=line") == 0) {
+        line_test ();
+    } else {
+        perror ("Not using line test");
+    }
 
-	std::string imagePath = "image.bmp";
-	SDL_Surface* bmp = SDL_LoadBMP(imagePath.c_str());
-	if (bmp == nullptr) {
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
+    if (strcmp (test, "--test=image") == 0) {
+        image_test ();
+        perror ("Not using image test");
+    } else {
 
-	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
-	SDL_FreeSurface(bmp);
-	if (tex == nullptr) {
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
+    }
 
-	//A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
-	for (int i = 0; i < 3; ++i) {
-		//First clear the renderer
-		SDL_RenderClear(ren);
-		//Draw the texture
-		SDL_RenderCopy(ren, tex, NULL, NULL);
-		//Update the screen
-		SDL_RenderPresent(ren);
-		//Take a quick break after all that hard work
-		SDL_Delay(1000);
-	}
+    if (strcmp (test, "--test=pixel") == 0) {
+        pixel_test ();
+    } else {
+        perror ("Valid test not found");
+        return exit () || errnum;
+    }
 
-	SDL_DestroyTexture(tex);
-	SDL_DestroyRenderer(ren);
-	SDL_DestroyWindow(win);
-	SDL_Quit();
-	return 0;
+    while (true) {
+        errnum == input ();
+        if (errnum != 0) {
+            return exit () || errnum;
+        }
+        errnum == SDL_RenderClear (renderer);
+        if (errnum != 0) {
+            return exit () || errnum;
+        }
+        errnum == SDL_RenderCopy (renderer, texture, NULL, NULL);
+        if (errnum != 0) {
+            return exit () || errnum;
+        }
+        errnum == SDL_SetRenderDrawColor (renderer, 255, 255, 255, 255);
+        if (errnum != 0) {
+            return exit () || errnum;
+        }
+        errnum == SDL_RenderDrawLine (renderer, 0, 0, WIDTH, HEIGHT);
+        if (errnum != 0) {
+            return exit () || errnum;
+        }
+        SDL_RenderPresent (renderer);
+        SDL_Delay (1000);
+    }
+
+    return exit () || errnum;
 }
 
-void print_header(const char* header) {
-	std::cout << "\n" << header << std::endl;
+
+int input () {
+    while (SDL_PollEvent (&event)) {
+        switch (event.type) {
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym) {
+                    case SDLK_q:
+                        return -1;
+                    default:
+                        break;
+                }
+        }
+    }
+    return 0;
 }
 
-void pixel_test() {
-	pixel p1;
-	p1.init(0, 0, 0, 255);
-	print_header("INIT");
-	printf("red: %u\n", p1.r);
-	printf("green: %u\n", p1.g);
-	printf("blue: %u\n", p1.b);
-	printf("alpha: %u\n", p1.a);
+int setup () {
+    if (SDL_Init (SDL_INIT_VIDEO) != 0) {
+        fprintf (stderr, "Video initialization failed: %s\n",
+                 SDL_GetError ());
+        SDL_Quit ();
+        return 1;
+    }
 
-	pixel p2;
-	p2.init(0, 0, 0, 255);
-	p2.set(0xFF0000FF);
-	print_header("SET RED");
-	printf("red: %u\n", p2.r);
-	printf("green: %u\n", p2.g);
-	printf("blue: %u\n", p2.b);
-	printf("alpha: %u\n", p2.a);
+    SDL_GL_SetAttribute (SDL_GL_RED_SIZE, 5);
+    SDL_GL_SetAttribute (SDL_GL_GREEN_SIZE, 5);
+    SDL_GL_SetAttribute (SDL_GL_BLUE_SIZE, 5);
+    SDL_GL_SetAttribute (SDL_GL_DEPTH_SIZE, 16);
+    SDL_GL_SetAttribute (SDL_GL_DOUBLEBUFFER, 1);
 
-	BYTE* byte_arr = p1.pack();
-	print_header("PACKED");
-	printf("red: %u\n", byte_arr[2]);
-	printf("green: %u\n", byte_arr[1]);
-	printf("blue: %u\n", byte_arr[0]);
-	printf("alpha: %u\n", byte_arr[3]);
+    window = SDL_CreateWindow ("PixelPerfect", 100, 100, WIDTH, HEIGHT, FLAGS);
+    if (window == nullptr) {
+        fprintf (stderr, "Video mode set failed: %s\n",
+                 SDL_GetError ());
+        SDL_Quit ();
+        return 1;
+    }
 
-	print_header("SHIFT");
-	int hex_value = 0xFFFFFF;
-	printf("%d >> 8 = %d", hex_value, hex_value >> 8);
+    renderer = SDL_CreateRenderer (window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == nullptr) {
+        SDL_DestroyWindow (window);
+        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError () << std::endl;
+        SDL_Quit ();
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor (renderer, 0, 0, 0, 255);
+
+    return 0;
+}
+
+int exit () {
+    SDL_DestroyTexture (texture);
+    SDL_DestroyRenderer (renderer);
+    SDL_DestroyWindow (window);
+    SDL_Quit ();
+    return 0;
+}
+
+int line_test () {
+    return 0;
+}
+
+int pixel_test () {
+    pixel p1;
+    p1.init (0, 0, 0, 255);
+    printf ("INIT\n");
+    printf ("red: %u\n", p1.r);
+    printf ("green: %u\n", p1.g);
+    printf ("blue: %u\n", p1.b);
+    printf ("alpha: %u\n", p1.a);
+
+    pixel p2;
+    p2.init (0, 0, 0, 255);
+    p2.set (0xFF0000FF);
+    printf ("SET RED\n");
+    printf ("red: %u\n", p2.r);
+    printf ("green: %u\n", p2.g);
+    printf ("blue: %u\n", p2.b);
+    printf ("alpha: %u\n", p2.a);
+
+    BYTE* byte_arr = p1.pack ();
+    printf ("PACKED\n");
+    printf ("red: %u\n", byte_arr[2]);
+    printf ("green: %u\n", byte_arr[1]);
+    printf ("blue: %u\n", byte_arr[0]);
+    printf ("alpha: %u\n", byte_arr[3]);
+
+    printf ("SHIFT\n");
+    int hex_value = 0xFFFFFF;
+    printf ("%d >> 8 = %d", hex_value, hex_value >> 8);
+    return 0;
+}
+
+int image_test () {
+    std::string imagePath = "image.bmp";
+    bmp = SDL_LoadBMP (imagePath.c_str ());
+    if (bmp == nullptr) {
+        SDL_DestroyRenderer (renderer);
+        SDL_DestroyWindow (window);
+        std::cout << "SDL_LoadBMP Error: " << SDL_GetError () << std::endl;
+        SDL_Quit ();
+        return 1;
+    }
+
+    texture = SDL_CreateTextureFromSurface (renderer, bmp);
+    SDL_FreeSurface (bmp);
+    if (texture == nullptr) {
+        SDL_DestroyRenderer (renderer);
+        SDL_DestroyWindow (window);
+        std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError () << std::endl;
+        SDL_Quit ();
+        return 1;
+    }
+
+    return 0;
 }
